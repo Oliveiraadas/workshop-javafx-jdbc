a627b053efa80446bddb2c174d92b9eb529013de
Oliveiraadas <oliveiraadas@gmail.com>
2026-05-29 09:50:49 -0300

    Department view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,6 @@
 package application;
-	
-import java.io.IOException;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,24 +8,33 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  
-	@Override
-	public void start(Stage primaryStage) {
-	    try {
-	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-	        Parent parent = loader.load();
-	        Scene mainScene = new Scene(parent);
-	        primaryStage.setScene(mainScene);
-	        primaryStage.setTitle("Sample JavaFX application");
-	        primaryStage.show();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	}
 
-	
+    // 1. Garanta que essa variável estática exista para armazenar a cena
+    private static Scene mainScene;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent parent = loader.load();
+            
+            // 2. CORREÇÃO: Guarde a referência na variável estática de classe (sem o "Scene" na frente)
+            mainScene = new Scene(parent);
+            
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 3. Garanta que o método GET retorne a variável estática correta
+    public static Scene getMainScene() {
+        return mainScene;
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
-
 }
